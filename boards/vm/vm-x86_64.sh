0cb2c821e368bd01b00963f30aad544acd3261cd
Sejin Hwang <sejin89x@gmail.com>
2022-10-19 18:42:15 -0700

    Multiple VM mode support

--- boards/vm/vm-x86_64.sh
+++ boards/vm/vm-x86_64.sh
@@ -1,8 +1,8 @@
 QEMU=qemu-system-x86_64
 
-isoname=output.iso
 
 BDDIR=.
+ISONAME=$BDDIR/output.iso
 SCRIPTF=$BDDIR/qemu-if.script
 EXT4HDD=$BDDIR/image.ext4
 EXT4CFG=$BDDIR/config.ext4
@@ -11,7 +11,7 @@ run_qemu() {
     $QEMU -m 4096 -nographic -smp 4 \
         -device e1000,netdev=net0,mac=$1 \
         -netdev tap,id=net0,script=$SCRIPTF \
-        -cdrom $isoname \
+        -cdrom $ISONAME \
         -drive file=$EXT4HDD \
         -drive file=$EXT4CFG
 }
@@ -25,6 +25,17 @@ case "$1" in
     "stop")
         sudo killall $QEMU
         ;;
+    "publish")
+        if [ -f $EXT4HDD -a \
+                -f $EXT4CFG -a \
+                -f $ISONAME ]; then 
+            [ -d $2 ] || mkdir -p $2
+            cp -rf $SCRIPTF $2
+            cp -rf $EXT4HDD $2
+            cp -rf $EXT4CFG $2
+            cp -rf $ISONAME $2
+            cp -rf $0       $2
+        fi
 esac
 
 
