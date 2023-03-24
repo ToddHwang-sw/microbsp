5fceb8ed09bc97d6ef7d11c460784da528384485
Sejin Hwang <sejin89x@gmail.com>
2023-03-23 22:46:08 -0700

    Host network configuration

---
+++ hostnet/runap.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+##
+## hostapd_g.conf - 11G mode
+## hostapd.conf   - 11N mode
+##
+HOSTAP_N_CONF=./hostapd.conf
+HOSTAP_G_CONF=./hostapd_g.conf
+
+## Fake job
+sudo ufw disable
+sudo ufw enable 
+sudo ufw disable
+sudo iptables -t nat -L -v
+sleep 1
+
+sudo brctl addbr br0
+
+\rm -rf ./hostap*.log
+
+## N/A AP
+sudo hostapd -ddddd -B $HOSTAP_N_CONF > ./hostap_n.log
+
+## G AP
+sudo hostapd -ddddd -B $HOSTAP_G_CONF > ./hostap_g.log
+sleep 1
+sudo ifconfig br0 up
+sudo ifconfig br0 10.5.5.1 netmask 255.255.255.0 up
+
+# DNS
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+
+if [ -s /etc/resolv.conf ]; then
+    sudo unlink /etc/resolv.conf
+    echo nameserver 8.8.8.8 | sudo tee /etc/resolv.conf
+fi
+
+sudo systemctl restart dnsmasq
+
